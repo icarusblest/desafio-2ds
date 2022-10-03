730ed074aa6549d9f2bf8def66af256c49797af7
Vinicius <63076142+icarusblest@users.noreply.github.com>
2022-10-03 16:29:01 -0300

    Add files via upload

---
+++ desafio/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Globalization;
+
+namespace desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         
+
+            Console.Write("\t\tInforme o valor do Salário Mínimo: ");
+            double salario_minimo = Double.Parse(Console.ReadLine());
+            Console.WriteLine();    // Adiciona uma linha em branco
+
+            // Solicita e lê o número de dependentes do Contribuinte
+            Console.Write("\t\tInforme a Quantidade de Dependentes: ");
+            int ndep = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();    // Adiciona uma linha em branco
+            Console.Write("\t\tInforme o valor da Renda Mensal do Contribuinte: ");
+
+            double renda_mensal = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine();    // Adiciona uma linha em branco​​
+
+            // Efetua o cálculo do Desconto do Salário Mínimo por Dependente​
+
+            // O desconto por dependente será de 5% do salário mínimo​
+
+            double desconto = (ndep * 0.05 * salario_minimo);
+
+            Console.WriteLine("\t\tDesconto igual a: {0}", desconto);
+
+            Console.WriteLine();    // Adiciona uma linha em branco​
+            double renda_liquida = (renda_mensal - desconto);
+
+            Console.WriteLine("\t\tRenda líquida igual a: {0}", renda_liquida);
+
+            Console.WriteLine();    // Adiciona uma linha em branco​​
+
+            /* AlíquotasAlíquota​
+
+ * Até 2 salários mínimos isento​
+
+ * 2 a 3 salários mínimos5%​
+
+ * 3 a 5 salários mínimos10%​
+
+ * 5 a 7 salários mínimos15%​
+
+ * acima de 7 salários mínimos20%​
+ */
+
+            if ((renda_liquida) <= (salario_minimo * 2))    // Utilize chaves para mais de uma instrução (blocos de instruções)​
+
+            {
+                  
+                double imposto_renda = 0;
+
+                Console.WriteLine("\t\tO valor do imposto de renda do contribuinte é: {0}", imposto_renda);
+
+                Console.WriteLine();    // Adiciona uma linha em branco​
+            }
+
+            else
+
+            {
+
+                if ((renda_liquida) <= (salario_minimo * 3))
+
+                {
+
+                    double imposto_renda = (renda_liquida * 0.05);
+
+                    Console.WriteLine("\t\tO valor do imposto de renda do contribuinte é: {0}", imposto_renda);
+
+                    Console.WriteLine();    // Adiciona uma linha em branco​
+
+                }
+                
+
+
+               else
+
+                {
+
+                    if ((renda_liquida) <= (salario_minimo * 5))
+
+                    {
+
+                       double imposto_renda = (renda_liquida * 0.10);
+
+                        Console.WriteLine("\t\tO valor do imposto de renda do contribuinte é: {0}", imposto_renda);
+
+                        Console.WriteLine();    // Adiciona uma linha em branco​
+
+                    }
+
+                    else
+
+                    {
+
+                        if ((renda_liquida) <= (salario_minimo * 7))
+
+                        {
+
+                            double imposto_renda = (renda_liquida * 0.15);
+
+                            Console.WriteLine("\t\tO valor do imposto de renda do contribuinte é: {0}", imposto_renda);
+
+                            Console.WriteLine();    // Adiciona uma linha em branco​
+
+                        }
+                    
+
+                        else
+
+                        {
+
+                            double imposto_renda = (renda_liquida * 0.20);
+
+                            Console.WriteLine("\t\tO valor do imposto de renda do contribuinte é: {0}", imposto_renda);
+
+                            Console.WriteLine();    // Adiciona uma linha em branco​
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+        }
+    }
+        
+    
---
+++ desafio/Rendas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desafio
+{
+    class Rendas
+    {
+        public double Salario_minimo { get; private set; } // Salário Mínimo do Contribuinte​
+          public double Renda_mensa { get; private set; }   // Renda Mensal do Contribuinte​
+        public double Desconto { get; private set; }      // Desconto a ser efetuado do Imposto para cada dependente do Contribuinte​
+        public double Renda_liquida { get; private set; }  // É a Renda Mensal menos o Desconto​
+        public double Imposto_renda { get; private set; } // Imposto de Renda do Contribuinte​
+        public int Ndep { get; private set; }
+
+
+        public Rendas(double renda_mensa, double desconto, double renda_liquida)
+        {
+      
+            Renda_mensa = renda_mensa;
+            Desconto = desconto;
+            Renda_liquida = renda_liquida;
+   
+
+        }
+
+        public Rendas(int ndep, double salario_minimo)
+        {
+            Ndep = ndep;
+            Salario_minimo = salario_minimo;
+            Imposto_renda = 0;
+        }
+
+        public double CalcDesconto()
+        {
+            return Ndep * 0.05 * Salario_minimo;
+        }
+
+        public double RendaLiq()
+        {
+            return Renda_mensa - Desconto;
+        }
+
+            
+
+
+
+    }
+}
